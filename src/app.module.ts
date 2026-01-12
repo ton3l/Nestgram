8a405c0c44bcb75814f322eab8dbb4233c408ed3
Elton <elton.felype.s.nascimento@gmail.com>
2026-01-12 15:35:35 -0300

    chore: revert app module to its original state, fix some package.json scripts and rename env files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,8 @@
-import { PrismaModule } from '@prisma-module/prisma.module';
 import { AppController } from '@nestgram/app.controller';
 import { AppService } from '@nestgram/app.service';
 import { Module } from '@nestjs/common';
 
 @Module({
-    imports: [PrismaModule],
     controllers: [AppController],
     providers: [AppService],
 })
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,12 +1,8 @@
-import { PrismaService } from '@prisma-module/prisma.service';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-    constructor(private readonly prisma: PrismaService) {}
-
-    async getHello() {
-    	console.log(await this.prisma.post.findMany());
+    getHello(): string {
         return 'Hello World!';
     }
 }
